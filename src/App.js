9f2ea869042dd266ec73255115977386da26188b
tbaronas <tomas.r.baronas@gmail.com>
2018-07-24 07:18:57 +0100

    feat(app): add react-native-vector-icons

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import translations from './i18n';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>{translations.t('home.homeScreen')}</Text>
+        <Icon name="rocket" size={30} color="#900" />
       </View>
     );
   }
