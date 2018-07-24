44857908b59366884f7e89ae8f2c5418ba56d0e2
tbaronas <tomas.r.baronas@gmail.com>
2018-07-24 06:52:28 +0100

    feat(app): add i18n

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
+import translations from './i18n';
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+        <Text>{translations.t('home.homeScreen')}</Text>
       </View>
     );
   }
