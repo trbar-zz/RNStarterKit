4707d6dd12c4f1fccbf918ffbe30d720d6289353
tbaronas <tomas.r.baronas@gmail.com>
2018-07-27 01:04:32 +0100

    feat(app): add splash, login and signUp screens with ui-kitten-kit styles

--- src/App.js
+++ src/App.js
@@ -1,50 +1,62 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { facebook, google } from 'react-native-simple-auth';
+import { View } from 'react-native';
+import { createDrawerNavigator, createStackNavigator } from 'react-navigation';
+import { withRkTheme } from 'react-native-ui-kitten';
+import { AppRoutes } from './config/navigation/routesBuilder';
+import * as Screens from './screens';
+import {bootstrap} from './config/bootstrap';
 
-import { FACEBOOK_CONFIG, GOOGLE_CONFIG } from './config/secrets'
-import translations from './i18n';
+bootstrap();
+//data.populateData();
 
-class HomeScreen extends React.Component {
-
-  onFacebookLoginButtonPress() {
-    facebook(FACEBOOK_CONFIG).then((info) => {
-      console.log('info', info)
-    }).catch((error) => {
-      console.log('error', error)
-    });
+function getCurrentRouteName(navigationState) {
+  if (!navigationState) {
+    return null;
   }
-
-  onGoogleLoginButtonPress() {
-    google(GOOGLE_CONFIG).then((info) => {
-      console.log('info', info)
-    }).catch((error) => {
-      console.log('error', error)
-    });
+  const route = navigationState.routes[navigationState.index];
+  if (route.routes) {
+    return getCurrentRouteName(route);
   }
+  return route.routeName;
+}
 
+console.log('Screens', Screens)
+
+let SideMenu = withRkTheme(Screens.SideMenu);
+const RNStarterApp = createStackNavigator({
+  First: {
+    screen: Screens.SplashScreen
+  },
+  Home: {
+    screen: createDrawerNavigator({
+        ...AppRoutes,
+      },
+      {
+        drawerOpenRoute: 'DrawerOpen',
+        drawerCloseRoute: 'DrawerClose',
+        drawerToggleRoute: 'DrawerToggle',
+        contentComponent: (props) => <SideMenu {...props}/>
+      })
+  }
+}, {
+  headerMode: 'none',
+});
+
+export default class App extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text testID='homeText'>{translations.t('home.homeScreen')}</Text>
-        <Icon name="rocket" size={30} color="#900" />
-        <Button
-          title="Facebook Login"
-          onPress={() => this.onFacebookLoginButtonPress()}
-        />
-        <Button
-          title="Google Login"
-          onPress={() => this.onGoogleLoginButtonPress()}
+      <View style={{flex: 1}}>
+        <RNStarterApp
+          onNavigationStateChange={(prevState, currentState) => {
+            const currentScreen = getCurrentRouteName(currentState);
+            const prevScreen = getCurrentRouteName(prevState);
+
+            if (prevScreen !== currentScreen) {
+              //track(currentScreen);
+            }
+          }}
         />
       </View>
     );
   }
 }
-
-export default createStackNavigator({
-  Home: {
-    screen: HomeScreen
-  },
-});
